04132bbc7b1725e0cb2b78ac96f1ea09a027f32d
Uglus <ugluss@gmail.com>
2025-08-03 11:22:21 +0300

    added sumLong and sumInt

--- src/Numbers.kt
+++ src/Numbers.kt
@@ -1,21 +1,62 @@
 interface Numbers {
 
-    fun sum(): Int
-
+    fun sumInt(): Int
+    fun sumLong(): Long
     fun difference(): Int
     fun divide() : Double
+    fun isSumInt() : Boolean
+    fun isSumLong() : Boolean
 
     class Base(
         private val number1: Int,
         private val number2: Int
         ) : Numbers{
-        override fun sum(): Int = number1 + number2
+
+        private var wasSumLongCalled: Boolean = false
+        private var wasSumLong: Boolean = false
+
+        override fun sumInt(): Int {
+            if(wasSumLongCalled) {
+                if (wasSumLong)
+                    throw IllegalStateException("you shouldn`t use this method: sumInt() ")
+                wasSumLongCalled = false
+                wasSumLong = false
+                return number1 + number2
+            }
+            throw IllegalAccessException("First you should check the sum by method isSumLong()")
+
+        }
+        override fun sumLong() : Long {
+            if(wasSumLongCalled) {
+                if (wasSumLong) {
+                    wasSumLongCalled = false
+                    wasSumLong = false
+                    return number1.toLong() + number2
+                }
+                throw IllegalStateException("you shouldn`t use this method: sumLong() ")
+            }
+            throw IllegalAccessException("First you should check the sum by method isSumLong()")
+
+        }
 
         override fun difference(): Int = number1 - number2
         override fun divide(): Double {
             if(number2==0)
                 throw IllegalArgumentException("На 0 ділити не можна")
             return number1.toDouble()/number2
+        }
+
+        override fun isSumInt(): Boolean {
+            val rest = Int.MAX_VALUE - number1
+            return number2 < rest
+        }
+
+        override fun isSumLong(): Boolean {
+            wasSumLongCalled = true
+            val rest = Int.MAX_VALUE - number1
+            val result = number2 > rest
+            wasSumLong = result
+            return result
         }
     }
 
--- test/NumbersTest.kt
+++ test/NumbersTest.kt
@@ -1,14 +1,61 @@
 import org.junit.Test
 import junit.framework.TestCase.assertEquals
-import kotlin.math.exp
 
 class NumbersTest {
+
     @Test
-    fun sum() {
-        val numbers : Numbers = Numbers.Base(number1 = 10, number2 = 5)
-        val actual = numbers.sum()
-        val expected = 15
+    fun test_sum_long(){
+        val numbers : Numbers = Numbers.Base(number1 = 2_000_000_000, number2 = 147_483_648)
+        val isSumLong = numbers.isSumLong()
+        val expected = true
+        assertEquals(expected,isSumLong)
+
+        val actual:Long = numbers.sumLong()
+        val expectedNumber : Long = Int.MAX_VALUE + 1L
+        assertEquals(expectedNumber,actual)
+    }
+
+    @Test
+    fun test_sum_int() {
+        val numbers : Numbers = Numbers.Base(number1 = 2_000_000_000, number2 = 147_483_647)
+        val isSumLong = numbers.isSumLong()
+        val expected = false
+        assertEquals(expected,isSumLong)
+
+        val actual:Int = numbers.sumInt()
+        val expectedNumber : Int = Int.MAX_VALUE
+        assertEquals(expectedNumber,actual)
+    }
+
+    @Test(expected = IllegalAccessException::class)
+    fun test_sum_int_no_check(){
+        val numbers : Numbers = Numbers.Base(number1 = 2_000_000_000, number2 = 2_000_000_000)
+        numbers.sumInt()
+    }
+
+    @Test(expected = IllegalAccessException::class)
+    fun test_sum_long_check(){
+        val numbers : Numbers = Numbers.Base(number1 = 2_000_000_000, number2 = 2_000_000_000)
+        numbers.sumLong()
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun test_sum_long_called_for_long_sum(){
+        val numbers : Numbers = Numbers.Base(number1 = 2_000_000_000, number2 = 2_000_000_000)
+        val actual = numbers.isSumLong()
+        val expected = true
         assertEquals(expected,actual)
+        numbers.sumInt()
+    }
+
+
+    @Test(expected = IllegalStateException::class)
+    fun test_sum_long_called_for_int_sum(){
+        val numbers : Numbers = Numbers.Base(number1 = 2_000_000_000, number2 = 1_000_000)
+        val actual = numbers.isSumLong()
+        val expected = false
+        assertEquals(expected,actual)
+        numbers.sumLong()
     }
 
     @Test
@@ -21,7 +68,7 @@ class NumbersTest {
 
     @Test
     fun test_divide(){
-        val numbers: Numbers = Numbers.Base(10,5);
+        val numbers: Numbers = Numbers.Base(10,5)
         val actual = numbers.divide()
         val expected = 2.0
         assertEquals(expected,actual,0.01)
@@ -38,7 +85,7 @@ class NumbersTest {
     @Test(expected = IllegalArgumentException::class)
     fun test_divide_zero(){
         val number: Numbers = Numbers.Base(number1= 12, number2 = 0)
-        number.divide();
+        number.divide()
     }
 
 }
